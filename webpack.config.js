56000654badffc6222a11f30e93e7d2124642b16
kwei <ywk991112@gmail.com>
2017-03-17 15:36:58 +0800

    npm init webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }, 
+      {
+        test: /\.s[ac]ss$/, 
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['sass-loader', 'css-loader'],
+        })
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  
+  plugins: [
+    new ExtractTextPlugin(), 
+    new webpack.LoaderOptionsPlugin({
+      minimize: inProduction;
+    })
+  ],
+};
+
+if(inProduction) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
