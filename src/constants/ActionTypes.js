f4c3edda103d121a8d1a7c2abb7c2673904f2207
ywk991112 <ywk991112@gmail.com>
2017-03-25 22:04:37 +0800

    add some action

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const CURRENT_CHANNEL = 'CURRENT_CHANNEL';
+export const SEND_MESSAGE = 'SEND_MESSAGE';
