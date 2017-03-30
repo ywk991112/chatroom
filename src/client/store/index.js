2fccb97756f093e3600fcdcb880d4506855cfe00
ywk991112 <ywk991112@gmail.com>
2017-03-30 19:32:41 +0800

    remove logger

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
 import reducer from '../reducers';
 import saga from '../sagas';
 
